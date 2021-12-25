52220e31d029de355f84ed742c70358d26276cc3
Ganesh Kasar <48873862+ganekasar@users.noreply.github.com>
2021-12-25 12:30:35 +0530

    1128 ms

---
+++ solutions/5. Longest Palindromic Substring.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string longestPalindrome(string X) {
+        int n = X.size();
+        string res;
+        
+        if(n==0)
+            return res;
+        
+        res.push_back(X[0]);
+        int maxLength = 1;
+        
+        for(int mid = 1; mid < n-1; mid++) {
+            
+            int len = 1;
+            string str;
+            str.push_back(X[mid]);
+            
+            for(int i=mid-1,j=mid+1; i>=0 && j<n; i--,j++) {
+                if(X[i] == X[j]) {
+                    str.insert(str.begin(), X[i]);
+                    str.push_back(X[j]);
+                    len += 2;
+                } else 
+                    break;
+            }
+            
+            if(len>maxLength){
+                maxLength = len;
+                res = str;
+            }
+        }    
+        
+        for(int mid=0; mid<n-1; mid++) {
+            if(X[mid] == X[mid+1]) {
+                int len = 2;
+                string str;
+                str.push_back(X[mid]);
+                str.push_back(X[mid+1]);
+                
+                for(int i=mid-1,j=mid+2; i>=0 && j<n; i--,j++) {
+                    if(X[i] == X[j]) {
+                        str.insert(str.begin(), X[i]);
+                        str.push_back(X[j]);
+                        len += 2;
