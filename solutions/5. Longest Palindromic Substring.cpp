cc75397c7224b0ffe84adc54171dc734387f1d6b
Ganesh Kasar <48873862+ganekasar@users.noreply.github.com>
2021-12-25 12:32:33 +0530

    736 ms

--- solutions/5. Longest Palindromic Substring.cpp
+++ solutions/5. Longest Palindromic Substring.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     string longestPalindrome(string X) {
         int n = X.size();
-        string res;
+        string answer;
         
-        if(n==0)
-            return res;
+        if(n == 0)
+            return answer;
         
-        res.push_back(X[0]);
+        answer.push_back(X[0]);
         int maxLength = 1;
         
         for(int mid = 1; mid < n-1; mid++) {
-            
             int len = 1;
             string str;
             str.push_back(X[mid]);
             
-            for(int i=mid-1,j=mid+1; i>=0 && j<n; i--,j++) {
+            for(int i = mid-1, j = mid+1; i >= 0 && j < n; i--, j++) {
                 if(X[i] == X[j]) {
                     str.insert(str.begin(), X[i]);
                     str.push_back(X[j]);
@@ -25,21 +24,35 @@ public:
                     break;
             }
             
-            if(len>maxLength){
+            if(len > maxLength) {
                 maxLength = len;
-                res = str;
+                answer = str;
             }
         }    
         
-        for(int mid=0; mid<n-1; mid++) {
+        for(int mid = 0; mid < n-1; mid++) {
             if(X[mid] == X[mid+1]) {
                 int len = 2;
                 string str;
                 str.push_back(X[mid]);
                 str.push_back(X[mid+1]);
                 
-                for(int i=mid-1,j=mid+2; i>=0 && j<n; i--,j++) {
+                for(int i = mid-1, j = mid+2; i >= 0 && j < n; i--, j++) {
                     if(X[i] == X[j]) {
                         str.insert(str.begin(), X[i]);
                         str.push_back(X[j]);
                         len += 2;
+                    } else 
+                        break;
+                }
+                
+                if(len > maxLength) {
+                    maxLength = len;
+                    answer = str;
+                }
+            } 
+        }
+        
+        return answer;
+    }
+};
